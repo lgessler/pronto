f9c6214e5cbd791abfd2f0d0e413b866805facb8
Luke Gessler <lukegessler@gmail.com>
2023-04-15 15:08:54 -0400

    bugfix

--- pronto/scripts/usfx_to_tsv.py
+++ pronto/scripts/usfx_to_tsv.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
         book = None
         chapter = None
         verse = None
+        verse_buffer = ""
         for child in xml.recursiveChildGenerator():
             name = child.name
             if name == "book":
@@ -26,5 +27,11 @@ if __name__ == "__main__":
                 chapter = child.attrs["id"]
             elif name == "v":
                 verse = child.attrs["id"]
+            elif name == "ve":
+                if book is not None and chapter is not None and verse is not None:
+                    f.write("\t".join([book, chapter, verse, verse_buffer]) + "\n")
+                verse = None
+                verse_buffer = ""
             elif name is None and not isinstance(child, bs4.element.ProcessingInstruction):
-                f.write("\t".join([book, chapter, verse, str(child)]) + "\n")
+                if book is not None and chapter is not None and verse is not None:
+                    verse_buffer += " " + str(child).strip()
