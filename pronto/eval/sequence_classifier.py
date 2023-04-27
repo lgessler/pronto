081f69a6c7c12cdb91606527948d5b9c82a405d1
Luke Gessler <lukegessler@gmail.com>
2023-04-26 23:17:34 -0400

    extend for pair classification

--- pronto/eval/sequence_classifier.py
+++ pronto/eval/sequence_classifier.py
@@ -1,4 +1,5 @@
 import os
+from logging import getLogger
 from random import seed, shuffle
 from tempfile import gettempdir
 
@@ -11,18 +12,35 @@ from evaluate import evaluator
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, Trainer,
                           TrainingArguments)
 
+logger = getLogger(__name__)
 
-def construct_dataset_dict(tsv_path, integer_label, max_integer_label):
+
+def construct_dataset_dict(
+    tsv_path,
+    integer_label,
+    max_integer_label,
+    text_column_index,
+    label_column_index,
+    second_text_column_index,
+    extra_input_column_index,
+):
     insts = []
     with open(tsv_path, "r") as f:
         for line in f:
             l = line.strip()
             if "\t" in l:
-                text, label = l.split("\t")
+                pieces = l.split("\t")
+                text = pieces[text_column_index]
+                label = pieces[label_column_index]
                 if integer_label:
                     label = int(label)
                     label = label if label < max_integer_label else max_integer_label
-                insts.append({"text": text, "label": label})
+                output = {"text": text, "label": label}
+                if second_text_column_index is not None:
+                    output["text2"] = pieces[second_text_column_index]
+                if extra_input_column_index is not None:
+                    output["extra"] = pieces[extra_input_column_index]
+                insts.append(output)
     seed(42)
     shuffle(insts)
 
@@ -31,12 +49,16 @@ def construct_dataset_dict(tsv_path, integer_label, max_integer_label):
     ddict2 = ddict["test"].train_test_split(test_size=0.5)
     ddict["dev"] = ddict2["train"]
     ddict["test"] = ddict2["test"]
-    ddict.features = datasets.Features(
-        {
-            "text": Sequence(feature=Value(dtype="string", id=None), length=-1, id=None),
-            "label": Sequence(feature=ClassLabel(names=list(set(dataset["label"])), id=None), length=-1, id=None),
-        }
-    )
+    features = {
+        "text": Sequence(feature=Value(dtype="string", id=None), length=-1, id=None),
+        "label": Sequence(feature=ClassLabel(names=list(set(dataset["label"])), id=None), length=-1, id=None),
+    }
+    if second_text_column_index is not None:
+        features["text2"] = Sequence(feature=Value(dtype="string", id=None), length=-1, id=None)
+    if extra_input_column_index is not None:
+        features["extra"] = Sequence(feature=ClassLabel(names=list(set(dataset["extra"])), id=None), length=-1, id=None)
+
+    ddict.features = datasets.Features(features)
     return ddict
 
 
@@ -47,13 +69,43 @@ def evaluate_model(
     integer_label=False,
     max_integer_label=None,
     lr=3e-5,
+    batch_size=16,
+    text_column_index=0,
+    label_column_index=1,
+    second_text_column_index=None,
+    extra_input_column_index=None,
+    max_sequence_length=None,
 ):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    dataset_dict = construct_dataset_dict(tsv_path, integer_label, max_integer_label)
+    dataset_dict = construct_dataset_dict(
+        tsv_path,
+        integer_label,
+        max_integer_label,
+        text_column_index,
+        label_column_index,
+        second_text_column_index,
+        extra_input_column_index,
+    )
     possible_labels = dataset_dict.features["label"].feature.names
     label2id = {v: i for i, v in enumerate(possible_labels)}
+    if extra_input_column_index is not None:
+        extra2id = {v: i for i, v in enumerate(dataset_dict.features["extra"].feature.names)}
 
-    tokenized_dataset_dict = dataset_dict.map(lambda x: tokenizer(x["text"], truncation=True), batched=True)
+    def tokenize(batch):
+        args = [batch["text"]]
+        if second_text_column_index is not None:
+            args.append(batch["text2"])
+        tokenizer_outputs = tokenizer(*args, truncation=True, max_length=max_sequence_length)
+        if extra_input_column_index is not None:
+            extra_ids = [extra2id[x] for x in batch["extra"]]
+            for k, vs in tokenizer_outputs.items():
+                for i, v in enumerate(vs):
+                    if len(v) == max_sequence_length:
+                        v.pop(-1)
+                    v.append(len(tokenizer.vocab) - extra_ids[i] - 1 if k == "input_ids" else 1)
+        return tokenizer_outputs
+
+    tokenized_dataset_dict = dataset_dict.map(tokenize, batched=True, batch_size=batch_size)
     if not integer_label:
         tokenized_dataset_dict = tokenized_dataset_dict.map(lambda x: {"label": label2id[x["label"]]})
     collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
@@ -78,8 +130,8 @@ def evaluate_model(
     training_args = TrainingArguments(
         output_dir=output_dir,
         learning_rate=lr,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         num_train_epochs=10,
         weight_decay=0.01,
         evaluation_strategy="epoch",
@@ -115,6 +167,17 @@ def evaluate_model(
 @click.option("--integer-label/--no-integer-label", default=False, help="Set to true if labels are integers")
 @click.option("--max-integer-label", default=None, type=int, help="Maximum value for labels if they are integers")
 @click.option("--lr", default=3e-5, type=float, help="Learning rate")
+@click.option("--batch-size", default=16, type=int, help="Batch size")
+@click.option("--text-column-index", default=0, type=int, help="TSV column which contains text")
+@click.option("--label-column-index", default=1, type=int, help="TSV column which contains sequence label")
+@click.option(
+    "--second-text-column-index",
+    default=None,
+    type=int,
+    help="TSV column which contains second sequence, if applicable",
+)
+@click.option("--extra-input-column-index", default=None, type=int, help="See use in code")
+@click.option("--max-sequence-length", default=512, type=int)
 def run(
     model_name,
     tsv_path,
@@ -122,6 +185,12 @@ def run(
     integer_label,
     max_integer_label,
     lr,
+    batch_size,
+    text_column_index,
+    label_column_index,
+    second_text_column_index,
+    extra_input_column_index,
+    max_sequence_length,
 ):
     print(evaluate_model(**locals()))
 
